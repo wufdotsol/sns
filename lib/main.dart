2d6578695e09a92bbdcd80490cd9e21968ddccd7
wuf.sol <wufdotsol@gmail.com>
2025-06-22 13:29:12 +0300

    firebase setup plus web hosting

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Wuf.sol Community App'),
     );
   }
 }
